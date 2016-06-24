650d906518ee2e8ccf23d3960bdbf02a4920e6dd
Joseph Fusco <hello@josephfus.co>
2016-06-23 23:33:38 -0400

    Stuff

---
+++ Gulpfile.js
@@ -0,0 +1,38 @@
+var gulp = require('gulp'),
+	sass = require('gulp-sass'),
+	notify = require("gulp-notify"),
+	uglify = require('gulp-uglify'),
+	rename = require("gulp-rename"),
+	jsonminify = require('gulp-jsonminify'),
+	watch = require('gulp-watch');
+
+gulp.task('styles', function () {
+	gulp.src('styles/**/*.scss')
+		.pipe(sass({outputStyle: 'compressed'}))
+		.pipe(rename({suffix: '.min'}))
+		.pipe(gulp.dest('dist'))
+		.pipe(notify("APFT styles are ready."));
+});
+
+gulp.task('js', function() {
+  	gulp.src('js/*.js')
+	    .pipe(uglify())
+	    .pipe(rename({suffix: '.min'}))
+	    .pipe(gulp.dest('dist'))
+	    .pipe(notify("APFT JavaScript is ready."));
+});
+
+gulp.task('data', function () {
+    return gulp.src(['data/*.json'])
+        .pipe(jsonminify())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('watch', function() {
+	gulp.watch('styles/**/*.scss', ['styles']);
+	gulp.watch('js/*.js', ['js']);
+	gulp.watch('data/*.json', ['data']);
+});
+
+gulp.task('default', ['styles', 'js', 'data', 'watch']);
---
+++ README.md
@@ -0,0 +1,9 @@
+# APFT
+
+Grading tool for the United States Army Physical Fitness Test.
+
+## Data
+
+Exercise standards can be found in standards.json. Original xls file used for initial conversion is also available.
+
+These are minified via gulp task `gulp data` and can be found in dist/
