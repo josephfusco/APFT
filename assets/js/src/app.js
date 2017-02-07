3ef932990c74d9e304c0074d381a9277e1d7df99
Joseph Fusco <hello@josephfus.co>
2017-02-07 14:37:21 -0500

    Update

--- assets/js/src/app.js
+++ assets/js/src/app.js
@@ -8,29 +8,38 @@
 		 */
 		$.getJSON('standards.json', function (data) {
 			standardsData = data;
+		}).done(function( data ) {
+			$('html').removeClass('loading');
 		});
 
 		/**
 		 * Calculate score.
 		 */
-		$('#calculator').submit(function (e) {
+		$('#calculator').submit(function () {
 			var g = $('#gender').val();
 			var a = $('#age').val();
 			var pu = $('#pu').val();
 			var su = $('#su').val();
 			var t = $('#min').val() + $('#sec').val();
 
-			// find the next lowest key for run time if there isn't an exact match
-			var runTimes = standardsData[g]['run'];
-			var lookup = lowerKeyFinder(runTimes);
-			t = lookup(t);
+			// change object keys to arrays for easier sorting
+			var puArr = Object.keys(standardsData[g]['push-ups']);
+			var suArr = Object.keys(standardsData[g]['sit-ups']);
+			var timeArr = Object.keys(standardsData[g]['run']);
+
+			// filter user reps to be within available range
+			var puFinal = getNextLowestKey(puArr, pu);
+			var suFinal = getNextLowestKey(suArr, su);
+
+			// filter user run time to be taken from available time
+			var runFinal = getNextHighestKey(timeArr, t);
 
 			// grab the score from the json data
-			var puScore = standardsData[g]['push-ups'][pu][0][a];
-			var suScore = standardsData[g]['sit-ups'][su][0][a];
-			var runScore = standardsData[g]['run'][t][0][a];
+			var puScore = standardsData[g]['push-ups'][puFinal][0][a];
+			var suScore = standardsData[g]['sit-ups'][suFinal][0][a];
+			var runScore = standardsData[g]['run'][runFinal][0][a];
 
-			// total up scores
+			// total all exercise scores
 			var totalScore = puScore + suScore + runScore;
 
 			// output results
@@ -43,35 +52,54 @@
 		});
 
 		/**
+		 * Keep keys within range and pick the next lowest if it doesn't exist.
+		 */
+		function getNextLowestKey(arr, val) {
+			var min = arr[0];
+			var max = arr[arr.length - 1];
+
+			for (var i = 0; i < arr.length; i++) {
+				var item = +arr[i];
+
+				if (item === val) {
+					return val;
+				} else if (min > +val) {
+					return min;
+				} else if (item > +val) {
+					return arr[i - 1];
+				} else if (item >= max) {
+					return max;
+				}
+			}
+		}
+
+		/**
+		 * Keep keys within range and pick the next highest if it doesn't exist.
+		 */
+		function getNextHighestKey(arr, val) {
+			var max = arr[arr.length - 1];
+
+			for (var i = 0; i < arr.length; i++) {
+				var item = +arr[i];
+
+				if (item === +val) {
+					return val;
+				} else if (item > +val) {
+					return arr[i];
+				} else if (item >= max) {
+					return max;
+				}
+			}
+		}
+
+		/**
 		 * Reset form.
 		 */
 		$('#reset').click(function () {
-			$(this).closest('form').find('input[type=number], input[type=text]').val('');
+			$(this).closest('#calculator').find('input[type=number], input[type=text]').val('');
 
 			return false; // prevent form submission
 		});
-
-		/**
-		 * Find the next lowest key.
-		 *
-		 * @link http://stackoverflow.com/questions/18410724/find-closest-lower-key-in-javascript-object
-		 */
-		var lowerKeyFinder = function (time) {
-			var keys = Object.keys(time);
-			keys.sort(function (a, b) {
-				return a - b;
-			});
-
-			return function (val) {
-				var maxKey = -1;
-				for (var i = 0, len = keys.length; i < len; i++) {
-					if (maxKey < 0 || keys[i] < val) {
-						maxKey = Math.max(maxKey, keys[i]);
-					}
-				}
-				return maxKey;
-			};
-		};
 
 	});
 
